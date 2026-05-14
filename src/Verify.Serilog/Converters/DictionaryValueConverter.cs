62568a57482a6a65c5c8215cc5015222dbc2b340
Simon Cropp <simon.cropp@gmail.com>
2026-05-14 19:00:06 +1000

    Update DictionaryValueConverter.cs

--- src/Verify.Serilog/Converters/DictionaryValueConverter.cs
+++ src/Verify.Serilog/Converters/DictionaryValueConverter.cs
@@ -2,5 +2,5 @@
     WriteOnlyJsonConverter<DictionaryValue>
 {
     public override void Write(VerifyJsonWriter writer, DictionaryValue value) =>
-        writer.Serialize(value.Elements.ToDictionary(_ => _.Key.Value!, _ => _.Value));
+        writer.Serialize(value.Elements.ToDictionary(_ => _.Key.Value ?? "null", _ => _.Value));
 }
